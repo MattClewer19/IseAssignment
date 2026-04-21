f991013e0012460af44f05882bad3577d727e992
MattClewer19 <mxc1144@student.bham.ac.uk>
2026-04-21 21:58:00 +0100

    Re-tested baseline

--- ResultsProcessing.py
+++ ResultsProcessing.py
@@ -28,6 +28,24 @@ def main():
 
             print(f"{file_name[:-4]} & {baseline_perf.mean():.4} & {tool_perf.mean():.4} & {'MyTool' if tool_perf.mean() < baseline_perf.mean() else 'Baseline'} & {p_value:.4} & {'Yes' if p_value < 0.05 else 'No'} \\\\")
 
+    print("")
+    print("=="*50)
+    print("")
+
+    for file_name in os.listdir(baseline_path):
+            if not file_name.endswith(".csv"):
+                continue
+
+            base_stats = analyse_file(os.path.join(baseline_path, file_name))
+            tool_stats = analyse_file(os.path.join(mytool_path, file_name))
+
+            baseline_perf = base_stats["Running Time"]
+            tool_perf = tool_stats["Running Time"]
+
+            _, p_value = mannwhitneyu(baseline_perf, tool_perf, alternative='two-sided')
+
+            print(f"{file_name[:-4]} & {baseline_perf.mean():.4} & {tool_perf.mean():.4} & {'MyTool' if tool_perf.mean() < baseline_perf.mean() else 'Baseline'} & {p_value:.4} & {'Yes' if p_value < 0.05 else 'No'} \\\\")
+
 
 if __name__ == "__main__":
     main()
